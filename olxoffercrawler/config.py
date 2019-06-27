d9dff2fab6bf41fbfc0d227237671215f43ca9fc
blooser <Blooser@protonmail.com>
2019-06-27 20:40:49 +0200

    Config module read valid urls from file
    Signed-off-by: blooser <Blooser@protonmail.com>

--- olxoffercrawler/config.py
+++ olxoffercrawler/config.py
@@ -1,1 +1,16 @@
-OLX_URLS = ["https://www.olx.pl/nieruchomosci/mieszkania/wynajem/poznan/?t=Trzypokojowe"]
+import re
+
+def is_url(url):
+    return re.match("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", url)
+
+def read_urls(filename): 
+    urls = []
+    with open(filename, "r") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if is_url(line):
+                urls.append(line)
+            else:
+                print(line, "is not a valid URL!")
+
+    return urls
