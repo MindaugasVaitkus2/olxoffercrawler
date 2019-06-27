d99818a5a46d5d80898b454fb757a65b93c99978
blooser <Blooser@protonmail.com>
2019-06-27 23:53:16 +0200

    Update
    Signed-off-by: blooser <Blooser@protonmail.com>

--- run_server.py
+++ run_server.py
@@ -1,4 +1,10 @@
 from webserver import server
 
-server.run()
+import os
+
+
+if __name__ == '__main__':
+	import os
+	port = int(os.environ.get('PORT', 5000))
+	server.run(host="0.0.0.0", port=port)
 
