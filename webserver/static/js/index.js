c77a7e76589a111c52b34a5aaaa9ff564bd5bcd1
blooser <Blooser@protonmail.com>
2019-06-27 23:01:04 +0200

    Allow to sort by price
    Signed-off-by: blooser <Blooser@protonmail.com>

--- webserver/static/js/index.js
+++ webserver/static/js/index.js
@@ -1,5 +1,11 @@
-function searchOffers(){
-	var pattern = $("#search").val()
+const doneTypingInterval = 300 
+const INFINITY = 1000000001
+
+var typingTimer;
+
+
+function sortOffersByName(){
+	var pattern = $("#search").val().toLowerCase()
 	var offers = document.getElementsByTagName("li") 
 
 	for(var i=0; i<offers.length; ++i){
@@ -14,6 +20,42 @@ function searchOffers(){
 	}
 }
 
+function sortOffersByPrice(){
+	var priceMin = $("#price-min").val() 
+	var priceMax = $("#price-max").val()
+
+	priceMin = (priceMin != "" && !isNaN(priceMin)) ? parseInt(priceMin) : -INFINITY
+	priceMax = (priceMax != "" && !isNaN(priceMax)) ? parseInt(priceMax) : INFINITY
+
+	var offers = document.getElementsByTagName("li") 
+
+	console.log("Min: ", priceMin)
+
+	for(var i=0; i<offers.length; ++i){
+		var price = parseInt(offers[i].getElementsByClassName("price")[0].textContent.replace(/\D/g,''));
+
+		if(price >= priceMin && price <= priceMax)
+			offers[i].style.display = ""
+		else
+			offers[i].setAttribute("style", "display: none !important")
+	}
+}
+
 $("#search").on("keyup", function(){
-	searchOffers()
+ 	clearTimeout(typingTimer)
+  	typingTimer = setTimeout(sortOffersByName, doneTypingInterval)
+})
+
+$("#search").on('keydown', function () {
+  clearTimeout(typingTimer)
+})
+
+$("#price-min, #price-max").on("keyup", function(){
+	clearTimeout(typingTimer)
+  	typingTimer = setTimeout(sortOffersByPrice, doneTypingInterval)
+})
+
+$("#price-min, #price-max").on("keydown", function(){
+	clearTimeout(typingTimer)
+  	typingTimer = setTimeout(sortOffersByPrice, doneTypingInterval)	
 })
