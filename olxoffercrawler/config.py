e1e985a4d4732fcc359a225de5816581c5b6f217
blooser <Blooser@protonmail.com>
2019-06-28 22:28:09 +0200

    Check if is valid OLX URL
    Signed-off-by: blooser <Blooser@protonmail.com>

--- olxoffercrawler/config.py
+++ olxoffercrawler/config.py
@@ -1,7 +1,11 @@
 import re
+import urllib.parse
 
 def is_url(url):
     return re.match("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", url)
+
+def is_olx_url(url):
+    return "olx" in urllib.parse.urlparse(url).hostname 
 
 def read_urls(filename): 
     urls = []
@@ -9,6 +13,10 @@ def read_urls(filename):
         for line in f.readlines():
             line = line.strip()
             if is_url(line):
+                if not is_olx_url(line):
+                    print(line, "is not a OLX URL")
+                    continue
+                    
                 urls.append(line)
             else:
                 print(line, "is not a valid URL!")
