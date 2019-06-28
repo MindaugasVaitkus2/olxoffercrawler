0504d91d874781103827bc163f7980e33b454e38
blooser <Blooser@protonmail.com>
2019-06-28 22:52:26 +0200

    Start thread timer with crawler
    Signed-off-by: blooser <Blooser@protonmail.com>

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from olxoffercrawler.config import read_urls
 
 from webserver import server
 
-urls = read_urls("URLS")
-crawler = Crawler(urls)
+def run_crawler_and_start_new_timer():    
+    urls = read_urls("URLS")
+    crawler = Crawler(urls)
+    threading.timer(18000000, crawler.crawl).start()
 
-threading.Timer(18000000, crawler.crawl).start()
+threading.Timer(18000000, run_crawler_and_start_new_timer).start()
 
 server.run()
