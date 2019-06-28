a2157cf3f9e4daf635ca28ce50b6b76c552020d5
blooser <Blooser@protonmail.com>
2019-06-28 23:05:10 +0200

    Update
    Signed-off-by: blooser <Blooser@protonmail.com>

--- app.py
+++ app.py
@@ -12,4 +12,7 @@ def run_crawler_and_start_new_timer():
 
 threading.Timer(18000000, run_crawler_and_start_new_timer).start()
 
-server.run()
+if __name__ == '__main__':
+	import os
+	port = int(os.environ.get('PORT', 5000))
+	server.run(host="0.0.0.0", port=port)
--- run_server.py
+++ run_server.py
@@ -1,10 +1,7 @@
 from webserver import server
 
-import os
-
 
 if __name__ == '__main__':
 	import os
 	port = int(os.environ.get('PORT', 5000))
 	server.run(host="0.0.0.0", port=port)
-
