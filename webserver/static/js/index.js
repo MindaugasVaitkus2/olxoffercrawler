376e32a6b73fca06e4f32871ecb99250a3b6f77a
blooser <Blooser@protonmail.com>
2019-06-29 13:42:48 +0200

    Scroll to up after clicked pagination object
    Signed-off-by: blooser <Blooser@protonmail.com>

--- webserver/static/js/index.js
+++ webserver/static/js/index.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
-	$('#offers-table').DataTable();
+	$('#offers-table').DataTable()
+	$("#offers-table").on('page.dt', function() {
+  		$('html, body').animate({
+    	scrollTop: $(".dataTables_wrapper").offset().top
+  		}, 'slow');
+	});
 })
+
+$(document).on("click", function(){
+	window.scrollTo(0, 0)
+}, ".paginate_button")
 
 $(".img-thumbnail").on("click", function() {
    $('#imagepreview').attr('src', $(this).attr('src'));
