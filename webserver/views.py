edee4d97e50283e4ec3a270d73d2cb3638544715
blooser <Blooser@protonmail.com>
2019-06-30 00:53:46 +0200

    Show last run
    Signed-off-by: blooser <Blooser@protonmail.com>

--- webserver/views.py
+++ webserver/views.py
@@ -6,8 +6,11 @@ from flask import(
 from webserver.database import db_session
 from webserver.models import OfferModel
 
+from olxoffercrawler.logger import Logger
+
 
 @server.route("/", methods=["GET", "POST"])
 def index():	
-	offers = OfferModel.query.all()	
-	return render_template("index.html", offers=offers)
+    offers = OfferModel.query.all()	
+    last_run = Logger.get_last_run()
+    return render_template("index.html", offers=offers, last_run=last_run)
