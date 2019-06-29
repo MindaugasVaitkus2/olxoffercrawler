00c1e58d7f8b3b533d2dda58ccff49cdf5116190
blooser <Blooser@protonmail.com>
2019-06-29 12:27:59 +0200

    Run threaded
    Signed-off-by: blooser <Blooser@protonmail.com>

--- run_server.py
+++ run_server.py
@@ -4,4 +4,4 @@ from webserver import server
 if __name__ == '__main__':
 	import os
 	port = int(os.environ.get('PORT', 5000))
-	server.run(host="0.0.0.0", port=port)
+	server.run(host="0.0.0.0", port=port, threaded=True)
