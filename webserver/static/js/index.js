afb8fd433266844f31d5309d059f86cb604b8de9
blooser <Blooser@protonmail.com>
2019-06-29 14:31:37 +0200

    Insert events in datatable's row callback
    Signed-off-by: blooser <Blooser@protonmail.com>

--- webserver/static/js/index.js
+++ webserver/static/js/index.js
@@ -7,25 +7,36 @@ function copyToClipboard(link) {
 }
 
 $(document).ready(function(){
-	$('#offers-table').DataTable()
+	$('#offers-table').DataTable({
+		"fnRowCallback": function (nRow, aData, iDisplayIndex, iDisplayIndexFull) {
+			var $clipboard = $(nRow).find(".clipboard")
+
+			$clipboard.on("click", function(){
+				var link = $(this).parent().parent().parent().children("h2").children("a").attr("href")
+				copyToClipboard(link)
+
+				$(this).animate({opacity: 0.5},  250, function(){
+					$(this).animate({opacity: 1}, 250)
+				})
+			})		
+			
+			var $thumbnail = $(nRow).find(".img-thumbnail")
+
+			$thumbnail.on("click", function() {
+   				$('#imagepreview').attr('src', $(this).attr('src'));
+   				$('#imagemodal').modal('show'); 
+			});
+
+		}
+	})
+
 	$("#offers-table").on('page.dt', function() {
   		$('html, body').animate({
-    	scrollTop: $(".dataTables_wrapper").offset().top
+    		scrollTop: $(".dataTables_wrapper").offset().top
   		}, 'slow');
 	});
-
- 	$(".img-thumbnail").on("click", function() {
-   		$('#imagepreview').attr('src', $(this).attr('src'));
-   		$('#imagemodal').modal('show'); 
-	});
-
+	
 	$("#imagemodal").on("click", function(){
 		$(this).modal("hide")
 	})
-
-	$(".clipboard").on("click", function(){
-		var link = $(this).parent().parent().parent().children("h2").children("a").attr("href")
-		copyToClipboard(link)
-	})
-
 })
