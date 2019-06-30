2b22a23046910d94bd337a88004b343881701717
blooser <Blooser@protonmail.com>
2019-06-30 15:08:54 +0200

    Override __hash__
    Signed-off-by: blooser <Blooser@protonmail.com>

--- webserver/models.py
+++ webserver/models.py
@@ -7,18 +7,31 @@ from sqlalchemy import(
 
 
 class OfferModel(Base):
-	__tablename__ = "Offers"
+    __tablename__ = "Offers"
 
-	id = Column(Integer, primary_key=True, autoincrement=True)
-	url = Column(String(160))
-	title = Column(String(60), unique=False, nullable=False)
-	image = Column(String(120), nullable=False)
-	price = Column(Integer, nullable=False)
-	location = Column(String(60), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    url = Column(String(160))
+    title = Column(String(60), unique=False, nullable=False)
+    image = Column(String(120), nullable=False)
+    price = Column(Integer, nullable=False)
+    location = Column(String(60), nullable=False)
+
+    def __init__(self, url, title, image, price, location):
+        self.url = url
+        self.title = title
+        self.image = image
+        self.price = price
+        self.location = location
     
-	def __init__(self, url, title, image, price, location):
-		self.url = url
-		self.title = title
-		self.image = image
-		self.price = price
-		self.location = location
+    def __eq__(self, other):
+        return self.url == other.url
+
+    def __hash__(self):
+        return hash((
+            self.id,
+            self.url,
+            self.title,
+            self.image,
+            self.price,
+            self.location
+        ))
