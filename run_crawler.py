ba328cae7f60168402ddd86b3cf34821f6d70064
blooser <Blooser@protonmail.com>
2019-06-30 16:40:27 +0200

    Use BackgroundScheduler
    Signed-off-by: blooser <Blooser@protonmail.com>

--- run_crawler.py
+++ run_crawler.py
@@ -1,7 +1,14 @@
+from apscheduler.schedulers.background import BackgroundScheduler 
+
 from olxoffercrawler.crawler import Crawler
 from olxoffercrawler.config import read_urls
 
-urls = read_urls("URLS")
-crawler = Crawler(urls)
-crawler.crawl()    
+sched = BackgroundScheduler()
 
+@sched.scheduled_job("interval", hours=5)
+def start_crawler():
+    urls = read_urls("URLS")
+    crawler = Crawler(urls)
+    crawler.crawl()    
+
+sched.start()
